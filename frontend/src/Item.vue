aca6dbc10655a4f56f8b55c4bbd504552470b3ef
wsy <1902096702@qq.com>
2019-07-23 00:12:40 +0800

    修改勾选按钮为checkbox

--- frontend/src/Item.vue
+++ frontend/src/Item.vue
@@ -2,10 +2,11 @@
   <div id="item">
     <blockquote>Todo</blockquote>
     <ul>
-      <li><input placeholder="接下来做什么？" autofocus="true"></li>
-      <li v-for="item in todoList" :key="item.id" :class="{liFinished: finished}">
-        <button :class="{finish: finished}" @click="toggleFinish"></button>
-        {{item.content}}
+      <li><input refs="input" placeholder="接下来做什么？" autofocus="true" @keyup.13="new_todo()" v-model="text"></li>
+      <li v-for="(item,index) in todoList" :class="{finished: item.done}">
+        <input type="checkbox" id="item.id" v-model="item.done">
+        <label for="item.id"> {{item.text}}</label>
+        <button class="delete" @click="">X</button>
       </li>
     </ul>
   </div>
@@ -16,27 +17,28 @@ export default {
     data(){
       return {
         todoList:[
-          {id: 0, content: '做饭'},
-          {id: 1, content: '洗碗'},
-          {id: 2, content: '浇花'}
+          {id: 0, text: '做饭', done: false },
+          {id: 1, text: '吃饭', done: false },
+          {id: 2, text: '洗碗', done: false }
         ],
-        finished: false,
+        text: ''
       }
     },
   props:{
   },
   methods: {
-      toggleFinish: function () {
-        this.finished = !this.finished;
-      }
+    new_todo: function(){
+      this.todoList.push({id: 3, text: this.text, done: false});
+      // this.$refs.input. = '';
+    }
   }
 }
 </script>
 
 <style scoped>
-.finish{
-  background-color: #228be8;
-}
+/*.finish{*/
+  /*background-color: #228be8;*/
+/*}*/
 #item{
   float: right;
   width: 80%;
@@ -44,19 +46,22 @@ export default {
   border-right: 1px solid #7e7e81;
   background-color: #fff;
 }
-button{
-  width: 20px;
-  height: 20px;
-  border-radius:20px;
-  border: 1px solid #228be8;
-  margin-right: 10px;
-  outline: none;
-  background-color: white;
-}
 blockquote{
   margin-top: 50px;
 }
-.liFinished{
+.finished{
   text-decoration: line-through;
+}
+.delete{
+  width: 20px;
+  height: 20px;
+  border: none;
+  outline: none;
+  background-color: inherit;
+  color: rgba(205, 30, 20, 0.99);
+  font-weight: bold;
+  position: relative;
+  left: 85%;
+  font-size: 16px;
 }
 </style>
