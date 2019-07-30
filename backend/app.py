d0ee3b318250712de09072cba93cd965d9fdfb0b
ayu <o3o3oo@vip.qq.com>
2019-07-30 22:02:08 +0800

    Fix the 2do application
    1. fix get_todo interface

--- backend/app.py
+++ backend/app.py
@@ -78,11 +78,12 @@ def get_todo():
         "todo_list": []
     }
     for todo_list in TodoList.query.order_by(TodoList.timestamp.asc()).all():
+        todo_list_id = str(todo_list.id)
         todo_list_json = todo_list.to_json(include_relationship=True)
         todo_items = todo_list_json.pop("todo_items", "todo_items_key_error")
 
         data["todo_list"].append(todo_list_json)
-        data[todo_list.id] = todo_items
+        data[todo_list_id] = todo_items
     return jsonify(data)
 
 
